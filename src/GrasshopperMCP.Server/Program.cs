e7f3c1bfd3e9c8d637cc1468672c0bf93d1c3f3d
SOU <133685249+norioh-japan@users.noreply.github.com>
2025-03-26 11:22:14 +0900

    Add server entry point

---
+++ src/GrasshopperMCP.Server/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace GrasshopperMCP.Server
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseUrls("http://localhost:5000");
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddFile(hostingContext.Configuration.GetSection("Logging"));
+                });
+    }
+}
